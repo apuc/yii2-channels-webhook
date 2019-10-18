575a4319dd00ce202e049f8260173fe61357e287
kavalar <apuc06@mail.ru>
2019-10-18 12:39:04 +0300

    second commit

--- behaviors/WebHookBehavior.php
+++ behaviors/WebHookBehavior.php
@@ -11,25 +11,51 @@ use yii\web\HttpException;
 class WebHookBehavior extends Behavior
 {
     /**
-     * @var string|null url по которому будет отправляться запрос
+     * @var string url по которому будет отправляться запрос
      */
-    public $url = null;
+    public $url = 'https://webhooks.mychannels.gq';
 
     /**
      * @var array аттрибуты, которые будут отправляться вместе с запросом, по умолчанию отправляет все аттрибуты
      */
     public $attributes = [];
 
+    /**
+     * @var bool должен ли веб хук срабатывать при создании новой записи
+     */
+    public $onInsert = true;
+
+    /**
+     * @var bool должен ли веб хук срабатывать при изменении записи
+     */
+    public $onUpdate = true;
+
     public function events()
     {
-        return [
-            ActiveRecord::EVENT_AFTER_INSERT => 'throwHook',
-            ActiveRecord::EVENT_AFTER_UPDATE => 'throwHook',
-        ];
+        $events=[];
+        if($this->onInsert)
+            $events[ActiveRecord::EVENT_AFTER_INSERT] = 'throwHook';
+        if($this->onUpdate)
+            $events[ActiveRecord::EVENT_AFTER_UPDATE] = 'throwHook';
+        return $events;
     }
 
     public function throwHook()
     {
-        throw new HttpException(434, $this->url);
+        if($this->attributes === []){
+            $attributes = $this->owner->attributes;
+        } else {
+            $attributes=[];
+            foreach ($this->attributes as $attribute) {
+                $attributes[$attribute] = $this->owner->attributes[$attribute];
+            }
+        }
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $this->url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($attributes));
+        $response = curl_exec($curl);
+        curl_close($curl);
     }
 }
